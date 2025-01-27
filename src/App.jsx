f07576b998d53f4c2b91c1c57659c1bab9c52c05
Rajesh97266 <rajesh97266@gmail.com>
2025-01-27 15:10:53 +0530

    header-added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
+import Search from "./components/Search";
+
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline ">Hello world!</h1>
+    <div className="pattern">
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movie&apos;s</span>You&apos;ll
+            Enjoy Without Hassle
+          </h1>
+        </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        
+      </div>
     </div>
   );
 };
---
+++ src/components/Search.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/prop-types */
+const Search = ({ searchTerm, setSearchTerm }) => {
+  return <div className="search">
+    <div>
+        <img src="search.svg" alt="search" />
+        <input
+          type="text"
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+    </div>
+  </div>;
+};
+
+export default Search;
